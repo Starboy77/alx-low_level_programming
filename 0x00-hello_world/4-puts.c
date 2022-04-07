87a7f61175c139662b82fade7a512c42d0158c72
Starboy77 <ayomidemajoyegbe@gmail.com>
2022-04-07 13:23:30 +0100

    4-puts.c modified 1

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -7,7 +7,7 @@
  */
 int main(void)
 {
-   char Mystr[] ="\"Programming is like building a multilingual puzzle\n";
-   puts(Mystr);
-   return (0);
+char Mystr[] = "\"Programming is like building a multilingual puzzle";
+puts(Mystr);
+return (0);
 }
