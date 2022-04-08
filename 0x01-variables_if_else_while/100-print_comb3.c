4aca0eff82a8624b8b7d10af46d00bc13bda9db4
Starboy77 <ayomidemajoyegbe@gmail.com>
2022-04-08 21:50:53 +0100

    100-print_comb3.c file updated 2

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -6,19 +6,21 @@
  */
 int main(void)
 {
-  int i = 0;
-  int j;
-  for (i = 0; i < 9; i++)
-    {
-      for (j = i + 1; j < 10; j++)
-	{
-	  putchar((i) + '0');
-	  putchar((j) + '0');
-	  putchar(',');
-	  putchar(' ');
-	}
-    }
-  putchar('\n');
-  
-    return (0);
+int i = 0;
+int j;
+for (i = 0; i < 9; i++)
+{
+for (j = i + 1; j < 10; j++)
+{
+putchar((i) + '0');
+putchar((j) + '0');
+if (i < 8)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+putchar('\n');
+return (0);
 }
