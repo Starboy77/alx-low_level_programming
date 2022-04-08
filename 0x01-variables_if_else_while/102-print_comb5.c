639e77a4cb20017ac3e68b4fe63622bd7d29c97c
Starboy77 <ayomidemajoyegbe@gmail.com>
2022-04-08 23:53:43 +0100

    102-print_comb5.c file added

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+  int i, j, k, l;
+  for (i = 0; i < 10; i++)
+    {
+      for (j = 0; j < 9; j++)
+	{
+	  for (k = i; k < 10; k++)
+	    {
+	      for (l = j + 1; l < 10; l++)
+		{
+		  putchar((i) + '0');
+		  putchar((j) + '0');
+		  putchar(' ');
+		  putchar((k) + '0');
+		  putchar((l) + '0');
+		  putchar(',');
+		  putchar(' ');
+		}
+	    }
+	}
+    }
+  putchar('\n');
+    return (0);
+}
