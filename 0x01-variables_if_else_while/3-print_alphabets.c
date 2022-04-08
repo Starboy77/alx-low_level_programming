b6a73d75709f484db2d642f2bbb5dd491395517c
Starboy77 <ayomidemajoyegbe@gmail.com>
2022-04-08 15:15:52 +0100

    3-print_alphabets file added

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char alpha = 'a';
+char Alpha = 'A';
+while (alpha <= 'z')
+{
+putchar(alpha);
+alpha++;
+}
+while (Alpha <= 'Z')
+{
+putchar(Alpha);
+ Alpha++;
+}
+putchar('\n');
+return (0);
+}
