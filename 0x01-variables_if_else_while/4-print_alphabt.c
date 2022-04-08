fbcd555e86553d22024460fd9119eb128c75ed15
Starboy77 <ayomidemajoyegbe@gmail.com>
2022-04-08 15:50:29 +0100

    4-print_alphabet.c file added

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char alpha = 'a';
+while (alpha <= 'z')
+{
+if (alpha = 'e')
+{
+continue;
+}
+if (alpha = 'q')
+{
+continue;
+}
+putchar(alpha);
+alpha++;
+}
+putchar('\n');
+return (0);
+}
