bd2c77fb3433a30566ede426735fb1d2795e57af
Starboy77 <ayomidemajoyegbe@gmail.com>
2022-04-08 17:43:10 +0100

    8-print_base16.c file added

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int num = 0;
+char alpha = 'a';
+while ( num < 10)
+{
+putchar((num) + '0');
+num++;
+}
+while (alpha < 'g')
+{
+putchar(alpha);
+alpha++;
+}
+putchar('\n');
+return (0);
+}
