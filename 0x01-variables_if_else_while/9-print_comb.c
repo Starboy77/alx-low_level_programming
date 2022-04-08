99834e0033f12bfe4b5aa19ec839e3fdc0f59b0e
Starboy77 <ayomidemajoyegbe@gmail.com>
2022-04-08 18:57:59 +0100

    9-print_comb.c file updated 2

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,19 +9,14 @@ int main(void)
 int num = 0;
 while (num < 10)
 {
+putchar((num) + '0');
 if (num < 9)
 {
-putchar((num) + '0');
 putchar(',');
 putchar(' ');
+}
 num++;
 }
-else
-{
-putchar((num) + '0');
-num++;
-}
-
-}
+putchar('\n');
 return (0);
 }
