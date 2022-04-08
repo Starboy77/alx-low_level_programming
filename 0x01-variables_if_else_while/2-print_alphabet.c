4efc88ceba6eead8d6bbb3819d9a0fe5935b78fc
Starboy77 <ayomidemajoyegbe@gmail.com>
2022-04-08 14:30:15 +0100

    2-print_alphabet file added

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char alpha = 'a';
+while (alpha <= 'z')
+{
+putchar(alpha);
+alpha++
+}
+return (0);
+}
