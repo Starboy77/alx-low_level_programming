7d5c8d143022ac6977a32cd44cee7bb0f58bfc66
starboy77 <ayomidemajoyegbe@gmail.com>
2022-04-12 13:43:35 +0100

    hotfix 4 for task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -13,3 +13,15 @@ int main(void)
 printput();
 return (0);
 }
+void printput(void)
+{
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+}
