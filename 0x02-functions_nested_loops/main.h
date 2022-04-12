dc02f90f17eba44d4bb0db3e2551896adf45f579
starboy77 <ayomidemajoyegbe@gmail.com>
2022-04-12 11:18:49 +0100

    0-putchar.c file added.

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,30 @@
+#ifndef header_file
+#define header_file
+#include <unistd.h>
+/**
+ * _putchar - print a single character.
+ * @c: character params.
+ * That's it.
+ * Return: character.
+ */
+int _putchar(char c)
+{
+return (write(1, &c, 1));
+}
+/**
+ * printput - Output "_putchar".
+ * That's it.
+ */
+void printput(void)
+{
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+}
+#endif
