dede7129cb122133492dbacb072a5f150a905ba4
starboy77 <ayomidemajoyegbe@gmail.com>
2022-04-12 14:06:45 +0100

    1-alphabet.c file added

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -9,3 +9,17 @@ int main(void)
 print_alphabet();
 return (0);
 }
+/**
+ * print_alphabet - print a-z
+ *
+ */
+void print_alphabet(void)
+{
+char alpha = 'a';
+while (alpha <= 'z')
+{
+_putchar(alpha);
+alpha++;
+}
+_putchar('\n');
+}
