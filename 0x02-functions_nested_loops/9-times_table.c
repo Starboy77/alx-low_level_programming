ddb43d795903d8e8962382c7dace2503520ea051
Starboy77 <ayomidemajoyegbe@gmail.com>
2022-04-13 02:00:33 +0100

    9-times_table.c file hot fix 1

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -12,8 +12,15 @@ for (i = 0; i < 10; i++)
 for (j = 0; j < 10; j++)
 {
 zero = i * j;
+if (zero < 10)
+{
+_putchar((zero) + '0');
+}
+if (zero >= 10)
+{
 _putchar((zero / 10) + '0');
 _putchar((zero % 10) + '0');
+}
 if (j < 9)
 {
 _putchar(',');
