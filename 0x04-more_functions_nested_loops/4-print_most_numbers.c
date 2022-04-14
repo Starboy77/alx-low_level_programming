094a483b039766d4287dd2d202532ab98bfd82ef
starboy77 <ayomidemajoyegbe@gmail.com>
2022-04-14 13:08:19 +0100

    4-print_most_numbers.c file fixed 1

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -6,12 +6,14 @@
  */
 void print_most_numbers(void)
 {
-	int i = 0;
+	int i;
 
-	for (i; i < 10; i++)
+	for (i = 0; i < 10; i++)
 	{
 		if (i == 2 || i == 4)
+		{
 			continue;
+		}
 		_putchar(i + '0');
 	}
 	_putchar('\n');
---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * more_numbers - check the code
+ *
+ */
+void more_numbers(void)
+{
+	int i, j;
+
+	for (
+}
