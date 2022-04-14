7addb55ea06eec0bb0669f7e703d8adda5bf8e5b
starboy77 <ayomidemajoyegbe@gmail.com>
2022-04-14 13:21:20 +0100

    5-more_numbers.c file modified

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,5 +8,13 @@ void more_numbers(void)
 {
 	int i, j;
 
-	for (
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 15; j++)
+		{
+			_putchar(j + '0');
+		}
+		_putchar('\n');
+	}
+	_putchar('\n');
 }
