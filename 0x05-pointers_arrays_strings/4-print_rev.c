02fa3d017c092bb7172d9cd33dbca0d97dd12e84
Starboy77 <ayomidemajoyegbe@gmail.com>
2022-04-20 20:33:22 +0100

    jsjsjd

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -5,15 +5,15 @@
  */
 void print_rev(char *s)
 {
-int​ i = ​0​;
-while​ (s[i] != 0)
+int i = 0;
+while (s[i] != 0)
 {
 i++;
 }
-while​ (i > 0)
+while (i > 0)
 {
 i--;
-_putchar​(s[i]);
+_putchar(s[i]);
 }
-_putchar​(​'​\n​'​);
+_putchar('\n');
 }
