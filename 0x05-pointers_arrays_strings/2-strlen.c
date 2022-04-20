65133f02ffa0a20336d4aeb5cc839927dc34e120
starboy77 <ayomidemajoyegbe@gmail.com>
2022-04-20 13:11:50 +0100

    2-strlen.c file added

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _strlen - check the code
+ * @s: pointer parameter.
+ * Return: Always 0.
+ */
+int _strlen(char *s)
+{
+	return (sizeof(s));
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -3,6 +3,7 @@
 
 void swap_int(int *a, int *b);
 int _putchar(char c);
-void reset_to_98(int *n);
+kvoid reset_to_98(int *n);
+int _strlen(char *s);
 
 #endif
