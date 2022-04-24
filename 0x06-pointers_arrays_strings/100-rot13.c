b69536262acc2e193d838efbd7a8a912e58873d7
Starboy77 <ayomidemajoyegbe@gmail.com>
2022-04-24 18:15:33 +0100

    101-print_number.c file added

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -7,12 +7,11 @@
 char *rot13(char *s)
 {
 int i = 0, j;
-char str[], rot[];
-str = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-rot = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+char str[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+char rot[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
 while (s[i] != '\0')
 {
-for (j = 0; j < 52; j++;)
+for (j = 0; j < 52; j++)
 {
 if (s[i] == str[j])
 {
---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_number - print number
+ * @n: int params
+ */
+void print_number(int n)
+{
+int i = 0, res = 0, k, j, l = 0;
+if (n < 0)
+{
+_putchar('-');
+}
+while (n != 0)
+{
+k = n % 10;
+n = n / 10;
+res = (res * 10) + k;
+i++
+}
+while (res != 0)
+{
+l = res % 10;
+res = res / 10;
+_putchar(l + '0');
+j++
+}
+}  
