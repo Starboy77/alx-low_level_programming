7b9d7415d88d4139bbfd64c9c79cb1c4ff429aaf
Starboy77 <ayomidemajoyegbe@gmail.com>
2022-04-24 13:29:40 +0100

    7-leet.c file hotfix 1

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -8,14 +8,17 @@
 
 char *leet(char *s)
 {
-int i = 0;
+int i = 0, j;
 char str[6] = {'a','e','o','t','l'};
 char num[5] = {'4','3','0','7','1'};
 while (s[i] != '\0')
 {
-if ((s[i] == str[i]) || (s[i] == (((str[i] - '0') - 32) + '0')))
+for (j = 0; j < 5; j++)
+{
+if ((s[i] == str[j]) || (s[i] == (((str[j] - '0') - 32) + '0')))
 {
 s[i] = num[i];
+}
 }
 i++;
 }
