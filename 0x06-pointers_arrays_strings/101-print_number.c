ab70eb009f26e8cd4c0f517eddd05dfaeffd1549
starboy77 <ayomidemajoyegbe@gmail.com>
2022-04-25 02:30:24 +0100

    101-print_number.c file updated 1

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -6,28 +6,16 @@
  */
 void print_number(int n)
 {
-int i = 0, res = 0, k, j, l = 0;
-if (n < 0)
-{
-_putchar('-');
-n = n * -1;
-}
-if (n == 0)
-{
-_putchar(n + '0');
-}
-while (n != 0)
-{
-k = n % 10;
-n = n / 10;
-res = (res * 10) + k;
-i++;
-}
-while (res != 0)
-{
-l = res % 10;
-res = res / 10;
-_putchar(l + '0');
-j++;
-}
+	unsigned int m = n;
+
+	if (m < 0)
+	{
+		_putchar('-');
+		m = -m;
+	}
+
+	if ((m / 10) > 0)
+		print_number(m / 10);
+
+	_putchar((m % 10) + '0');
 }
