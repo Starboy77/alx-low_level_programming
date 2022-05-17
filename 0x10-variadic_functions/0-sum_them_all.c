14ecf09ebdd99229b24d2e9938d071a63aeed7d5
starboy77 <ayomidemajoyegbe@gmail.com>
2022-05-17 01:38:02 +0100

    0-sum_them_all.c file added

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,25 @@
+#include "variadic_functions.h"
+#include <stdlib.h>
+#include <stdarg.h>
+
+/**
+ * sum_them_all - sum all argument
+ * @n: unsigned int
+ * Return: sum
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list ptr;
+	unsigned int i, sum = 0;
+
+	if (n == 0)
+		return (0);
+	va_start(ptr, n);
+	for (i = 0; i < n; i++)
+	{
+		sum += va_arg(ptr, int);
+	}
+	va_end(ptr);
+	return (sum);
+}
---
+++ 0x10-variadic_functions/README.md
@@ -0,0 +1,1 @@
+My ALX 0x10-variadic_functions projects
