c9065753fbfff081d905ee97eb34634b69440596
starboy77 <ayomidemajoyegbe@gmail.com>
2022-05-27 02:59:01 +0100

    5-free_listint2.c file added

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+#include <stdlib.h>
+
+
+/**
+ * free_listint2 - frees list
+ * @head: pointer to struct pointer
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *tmp, cu;
+
+	cu = *head;
+	if (head == NULL)
+		return;
+	while (cu)
+	{
+		tmp = cu->next;
+		free(cu);
+		cu = tmp;
+	}
+	head = NULL;
+}
