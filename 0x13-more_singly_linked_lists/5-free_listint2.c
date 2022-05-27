653ea5b5387c3d1df7183bf154561bde810724c0
starboy77 <ayomidemajoyegbe@gmail.com>
2022-05-27 03:06:12 +0100

    5-free_listint2.c file updated 1

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -9,16 +9,17 @@
 
 void free_listint2(listint_t **head)
 {
-	listint_t *tmp, *cu;
+	listint_t *tmp;
 
-	cu = *head;
 	if (head == NULL)
 		return;
-	while (cu)
+
+	while (*head)
 	{
-		tmp = cu->next;
-		free(cu);
-		cu = tmp;
+		tmp = (*head)->next;
+		free(*head);
+		*head = tmp;
 	}
+
 	head = NULL;
 }
