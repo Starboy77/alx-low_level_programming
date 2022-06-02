255e1ba4cd8e7cd3b171e0013b00fc5b384b2e7c
starboy77 <ayomidemajoyegbe@gmail.com>
2022-06-02 03:53:03 +0100

    1-create_file.c file added

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -19,5 +19,6 @@ typedef signed long int ssize_t;
 
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
 
 #endif /* MAIN_H_ */
