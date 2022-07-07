cdb37325cae9fee1cfde4704ca858fa1764f7342
starboy77 <ayomidemajoyegbe@gmail.com>
2022-07-07 21:11:45 +0100

    7-insert_dnodeint.c updated 6

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -21,7 +21,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	{
 		return (add_dnodeint(h, n));
 	}
-	if (idx == 1)
+	if (head->next == NULL)
 	{
 		return (add_dnodeint_end(h, n));
 	}
