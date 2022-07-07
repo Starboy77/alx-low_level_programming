e6f1d6b7ee480357c99cc7cc592a3eee99ca4532
starboy77 <ayomidemajoyegbe@gmail.com>
2022-07-07 15:47:49 +0100

    print list betty style solved

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,5 +1,12 @@
 #include "lists.h"
 
+
+/**
+ * print_dlistint - prints all lists data
+ * @h: head node
+ * Return: no of elements
+ */
+
 size_t print_dlistint(const dlistint_t *h)
 {
 	size_t n = 0;
